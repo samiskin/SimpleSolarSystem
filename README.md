5d046bfbd82dfe22527b73bf2132bdf08335c8b4
Shiranka Miskin <shiranka.miskin@gmail.com>
2015-07-18 19:48:29 -0400

    Done the planet display + stars, need text now

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# ReactBase
-A base to start off developing websites in React using various modern web technologies such as ES6 and Hotloading
+# Simple Solar System
+A simple simulation of the solar system, progressing at 60 days per second starting from August 20th 1997.  
+
+The scale of the planets are accurate relative to each other and the planet's orbits are also accurate, however the distance scale and planet size scale are different (so even if earth and venus might look so close together that they are touching, the distance between them is much larger than the radii would make it seem)
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -2,12 +2,50 @@
 var React = require('React');
 var _ = require('lodash');  // Needed for twojs
 var twojs = require('two.js');
+var Vec = require('Vec');
 
-var two = new Two({width: width, height: height});
+var width = window.innerWidth;
+var height = window.innerHeight;
+var two = new Two({ fullscreen: true });
+
+for (var i = 0; i < 300; i++) {
+  var x = Math.round(Math.random() * width * 2); // * 2 in case of zoom
+  var y = Math.round(Math.random() * height * 2);
+  var star = two.makeCircle(x, y, 1);
+  star.opacity = 0.5;
+  star.noStroke();
+}
 
 
-var width = 500;
-var height = 500;
+var scale = 60;
+var planetScale = 2;
+
+
+var sunRadius = 464.9; // in AU * 10^-5
+// var sun = two.makeCircle(0, 0, sunRadius * planetScale);
+var planets = {
+  "Mercury" : null,
+  "Venus"   : null,
+  "Earth"   : null,
+  "Mars"    : null,
+  "Jupiter" : null,
+  "Saturn"  : null,
+  "Uranus"  : null,
+  "Neptune" : null,
+  "Pluto"   : null
+};
+
+var planetGroup = two.makeGroup();
+
+
+
+
+var time = new Date();
+    time.setDate(21);
+    time.setMonth(5);
+    time.setYear(1997);
+    time.setHours(0);
+
 
 var data = require('data.json');
 
@@ -19,85 +57,85 @@ function cosd(angle) {
   return Math.cos(angle * (Math.PI / 180));
 }
 
-var Canvas = React.createClass({
-
-
+function isLeap(date) {
+  var y = date.getUTCFullYear();
+  return !(y % 4) && (y % 100) || !(y % 400);
+}
 
 //http://www.stargazing.net/kepler/ellipse.html
-  getPos(date) {
-    // Mars
-    var planet = "Mars";
-    var {i, o, p, a, n, e, L} = data.Planets[planet];
-    // Values from august 20th 1997
-    var dele = data.DaysSinceMonth["NormalYear"][7] + 20 + data.daysSinceJ2000["1997"];
-    console.log("dele: ", dele);
-    // Value wanted for 21st of June 1997
-    var dpos = data.DaysSinceMonth["NormalYear"][5] + 21 + data.daysSinceJ2000["1997"];
-    console.log("dpos: ", dpos);
-    var d= dpos - dele;
-    var hour = 0;
-    var minutes = 0;
-    d+= (hour + minutes/60)/24;
-     console.log("d: ", d);
+function getPosFromDate(planet, date) {
+  var dpos = data.DaysSinceMonth[isLeap(date) ? "LeapYear" : "NormalYear"][date.getMonth()]
+              + date.getDate()
+              + data.daysSinceJ2000[date.getFullYear()]
+              + (date.getHours() + date.getMinutes() / 60) / 24;
+  return getPos(dpos - date.planetDataDayCount);
+}
 
-    var M = n * d + L - p; // mean anomaly
-    while (M < 0) { M += 360; }
-    while (M > 360) { M -= 360; }
-    console.log("M: ", M);
-    var v = M + 180/Math.PI * [ (2 * e - e*e*e / 4) * sind(M) + 5/4 * e*e * sind(2*M) + 13/12 * e*e*e * sind(3*M)];
-    console.log("v: ", v);
+function getPos(planet, days) {
+  // Mars
+  var {i, o, p, a, n, e, L} = data.Planets[planet]; // n = sqrt(G(M + m)/a^3)
+  // Values from August 20th 1997
 
-    var r = a * (1 - e*e) / [1 + e*cosd(v)];
-    console.log("r: ", r);
+  var M = n * days + L - p; // mean anomaly
+  while (M < 0) { M += 360; }
+  while (M > 360) { M -= 360; }
+  var v = M + 180/Math.PI * [ (2 * e - e*e*e / 4) * sind(M) + 5/4 * e*e * sind(2*M) + 13/12 * e*e*e * sind(3*M)];
 
-    var x = r * [cosd(o) * cosd(v + p - o) - sind(o) * sind(v + p - o) * cosd(i)];
-    var y = r * [sind(o) * cosd(v + p - o) + cosd(o) * sind(v + p - o) * cosd(i)];
-    var z = r * [sind(v + p - o) * sind(i)];
+  var r = a * (1 - e*e) / [1 + e*cosd(v)];
 
-    console.log("Pos(x, y, z): ", x, y, z);
+  var x = r * [cosd(o) * cosd(v + p - o) - sind(o) * sind(v + p - o) * cosd(i)];
+  var y = r * [sind(o) * cosd(v + p - o) + cosd(o) * sind(v + p - o) * cosd(i)];
+  var z = r * [sind(v + p - o) * sind(i)];
 
-  },
+  return {x: x, y: y, z: z};
+}
+
+
+var Canvas = React.createClass({
 
   componentDidMount() {
 
-    this.getPos();
 
     var elem = React.findDOMNode(this.refs.twojs);
     two.appendTo(elem);
 
-    var circle = two.makeCircle(-70, 0, 50);
-    var rect = two.makeRectangle(70, 0, 100, 100);
-    circle.fill = '#FF8000';
-    circle.stroke = 'orangered';
-    rect.fill = 'rgba(0, 200, 255, 0.75)';
-    rect.stroke = '#1C75BC';
 
-    // Groups can take an array of shapes and/or groups.
-    var group = two.makeGroup(circle, rect);
 
-    // And have translation, rotation, scale like all shapes.
-    group.translation.set(two.width / 2, two.height / 2);
-    group.rotation = Math.PI;
-    group.scale = 0.75;
 
-    // You can also set the same properties a shape have.
-    group.linewidth = 7;
-    // Bind a function to scale and rotate the group
-    // to the animation loop.
-    two.bind('update', function(frameCount) {
-      // This code is called everytime two.update() is called.
-      // Effectively 60 times per second.
-      if (group.scale > 0.9999) {
-        group.scale = group.rotation = 0;
-      }
-      var t = (1 - group.scale) * 0.125;
-      group.scale += t;
-      group.rotation += t * 4 * Math.PI;
-    }).play();  // Finally, start the animation loop
+    var colors = {
+      "Mercury": '#9E9E9E',
+      "Venus": '#795548',
+      "Earth": '#2196F3',
+      "Mars": '#FF7043',
+      "Jupiter": '#E67E22',
+      "Saturn": '#A1887F',
+      "Uranus": '#4DB6AC',
+      "Neptune": '#3F51B5',
+      "Pluto": '#EABC98'
+    }
+
+    for (var planet in planets) {
+      var circle = two.makeCircle(0, 0, Math.max(data.Planets[planet].radius * planetScale, 2));
+      circle.fill = colors[planet];
+      planets[planet] = circle;
+      planetGroup.add(circle);
+    }
+    planetGroup.translation.set(two.width / 2, two.height / 2);
+    planetGroup.scale = 0.45;
+
+
+    two.bind('update', this.draw).play();  // Finally, start the animation loop
   },
 
-  draw() {
+  draw(frameCount) {
+    for (var planet in planets) {
+      var pos = getPos(planet,  frameCount + 4750);
+      pos = new Vec(pos.x, pos.y);
+      pos = pos.scale(scale);
+      planets[planet].translation.set(pos.x, pos.y);
+    }
 
+    planetGroup.translation.set(two.width / 2, two.height / 2);
 
   },
 
